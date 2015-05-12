25b331a18413ac3d7506735a3498456a57c3d7be
khoadp <dkhoa81@gmail.com>
2015-05-12 16:48:38 +0700

    Update send-sms.js

--- example/send-sms.js
+++ example/send-sms.js
@@ -1,11 +1,12 @@
 var userAccessToken = "USER_ACCESS_TOKEN"; // Get from kandy.getUserAccessToken(...)
 
-var to = "user2@domain.com"; // Required
+var from = "+1-408-475-****"; // Optional
+var to = "+1-408-475-****"; // Required
 var text = "Hello from Kandy"; // Required
 
 var kandy = new Kandy();
 
-kandy.sendIm(userAccessToken, to, text, function (data, response) {
+kandy.sendSms(userAccessToken, from, to, text, function (data, response) {
       var dataJson = JSON.parse(data);
       if (dataJson.message == "success") {
           console.log("Sent to " + to + ": " + text);
